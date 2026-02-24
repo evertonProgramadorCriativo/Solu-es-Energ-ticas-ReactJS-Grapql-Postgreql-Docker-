6e80a09d2ea6e1d5bec2157eb264ec0a48c268bc
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2026-02-24 20:43:36 -0300

    feat: Add form validation logic with podeCalcular flag and debug logging

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import { estadosData } from './data/estadosData';
 
 function App() {
 
-
-
-  const [estadoSelecionado, setEstadoSelecionado] = useState('RJ');
+  const [consumoMensal, setConsumoMensal] = useState('');
+  const [estadoSelecionado, setEstadoSelecionado] = useState('');
+  /**const [estadoSelecionado, setEstadoSelecionado] = useState('RJ');
   const estadoData = estadosData.find((e) => e.uf === estadoSelecionado);
   console.log('Estado selecionado:', estadoData);
-  /**
+  
    * Ao selecionar "RJ" no dropdown, o console exibirá:
    * { uf: 'RJ', nome: 'Rio de Janeiro', tarifaBaseKwh: 0.789 }
    */
+
+  const podeCadcular = estadoSelecionado !== '' && consumoMensal !== '' && Number(consumoMensal) > 0;
+  console.log('Pode calcular:', podeCadcular);
+  // Resultado do console: Pode calcular: false
   return (
     <div className="min-h-screen bg-cyan-400 flex flex-col">
       <Header />
