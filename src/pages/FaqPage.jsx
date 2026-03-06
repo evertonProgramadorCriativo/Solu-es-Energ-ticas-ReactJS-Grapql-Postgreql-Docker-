efaeb85ededec16af937525868ecb3bc101b3d43
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2026-03-06 16:44:55 -0300

    feat: adds decorative element (golden circle) to the FAQ header

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const FAQPage = () => {
   return (
-    <div className="bg-slate-50 min-h-[68vh]">
+    <div className="bg-slate-50 min-h-[100vh]">
       {/* ── Header ── */}
-      <header className="bg-slate-800 min-h-[520px] text-white py-14 relative overflow-hidden"></header>
+      <header className="bg-slate-800 min-h-[520px] text-white py-14 relative overflow-hidden">
+        {/* ── Bola no Canto Superior ── */}
+        <div className="absolute inset-0 min-h-52 opacity-10 ">
+          <div className="absolute top-0 right-0 w-80 h-80 bg-amber-400 rounded-full -translate-y-1/2 translate-x-1/2" />
+        </div>
+      </header>
     </div>
   );
 };
